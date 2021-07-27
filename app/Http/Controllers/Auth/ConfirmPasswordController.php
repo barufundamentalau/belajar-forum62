8dcbabc4f9c53c1f87ea4ac97c0dd01b77b765ce
CandraBlackz <blackz.wd@gmail.com>
2021-07-27 13:28:14 +0800

    delete route redirect home

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -26,12 +26,12 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
      *
-     * @return void
+     * @return void 
      */
     public function __construct()
     {
